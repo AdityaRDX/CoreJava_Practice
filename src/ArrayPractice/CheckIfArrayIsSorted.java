b00522eef3c7e0f9a1414b43588a53b0d43f8972
AdityaRDX <rakateaditya2002@gmail.com>
2025-01-14 13:55:39 +0530

    init

---
+++ src/ArrayPractice/CheckIfArrayIsSorted.java
@@ -0,0 +1,26 @@
+package ArrayPractice;
+
+public class CheckIfArrayIsSorted {
+
+	public static boolean isSorted(int[] arr, int idx) {
+		
+		if(arr[idx] ==arr.length-1) {
+			return true;
+		}
+		
+		if(arr[idx]< arr[idx+1]) {
+			return isSorted(arr, idx+1);
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5};
+		System.out.println(isSorted(arr, 0));
+		
+	}
+
+}
